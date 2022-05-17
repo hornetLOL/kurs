11bdbf19fe6f1ad437f98b631b66215359196952
hornetLOL <hornetlol192@gmail.com>
2022-05-17 16:47:28 +0300

    init commit

---
+++ src/main/java/ru/maks/kurs/KursApplication.java
@@ -0,0 +1,13 @@
+package ru.maks.kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/maks/kurs/KursApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maks.kurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
