c392abfd806593b2cea9f7dbbc783a4c51f40eea
Ilay Vinogradov <ilay125114@gmail.com>
2022-05-17 17:30:59 +0300

    commit 2

---
+++ src/main/java/ru/maks/kurs/dao/StudentsDao.java
@@ -0,0 +1,10 @@
+package ru.maks.kurs.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.maks.kurs.entity.Student;
+
+import java.util.Optional;
+
+public interface StudentsDao extends JpaRepository<Student, Long> {
+	Optional<Student> findByName(String name);
+}
---
+++ src/main/java/ru/maks/kurs/entity/Student.java
@@ -0,0 +1,4 @@
+package ru.maks.kurs.entity;
+
+public class Student {
+}
