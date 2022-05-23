cefdcd8cc7dd836d76aa83261a5a68f04abae756
Ilay Vinogradov <ilay125114@gmail.com>
2022-05-24 00:08:56 +0300

    Работающие мапперы + кнопка добавление и форма студента

--- src/main/java/ru/maks/kurs/web/controller/StaffController.java
+++ src/main/java/ru/maks/kurs/web/controller/StaffController.java
@@ -55,7 +55,7 @@ public class StaffController {
         return "staff/staff-list";
     }
 
-    @GetMapping()
+    @GetMapping
     public String getStaff(Model model, @RequestParam(name = "id", required = false) Long id,
                            @RequestParam(name = "numPass", required = false) Long numPass) {
         StaffDto staff;
